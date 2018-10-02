da1563f563764750210b9ecd9160a6dd753d9393
man55 <doukara001@gmail.com>
2018-10-02 11:43:16 +0100

    adding all files

---
+++ src/isoddnummber/Main.java
@@ -0,0 +1,39 @@
+
+package isoddnummber;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    private static int number ;
+    
+    public static void main(String[] args) {
+     
+
+        Scanner sc = new Scanner(System.in);
+        
+        
+        while(true){
+        try{
+            System.out.print("Enter the numeber : ");
+            number = sc.nextInt();
+         
+        }catch(Exception e)
+        {
+            
+           System.out.println("You must enter the number wihtout comma"); 
+           break;
+        }
+     
+        if(number %2 == 0)
+          System.out.println("Yes the number " + number + " is odd");                     
+        else 
+          System.out.println("No ! the number " + number + " is even");   
+        break;
+        }
+                   
+    
+    }
+    }   
+    
+
