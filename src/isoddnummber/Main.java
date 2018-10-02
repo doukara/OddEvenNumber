c547e1d2f7a93d3306dea580325f909f994b5e69
man55 <doukara001@gmail.com>
2018-10-02 11:48:27 +0100

    i change between odd and even

--- src/isoddnummber/Main.java
+++ src/isoddnummber/Main.java
@@ -26,9 +26,9 @@ public class Main {
         }
      
         if(number %2 == 0)
-          System.out.println("Yes the number " + number + " is odd");                     
+          System.out.println("The number " + number + " is even");                     
         else 
-          System.out.println("No ! the number " + number + " is even");   
+          System.out.println("The number " + number + " is odd");   
         break;
         }
                    
